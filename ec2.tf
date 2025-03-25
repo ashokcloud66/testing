35a5f39ebfe8209ca71169287eda7a757ec04089
ashokcloud66 <ashokcloud66@gmail.com>
2025-03-25 12:39:11 +0530

    2nd commit

--- ec2.tf
+++ ec2.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "apache_instance" {
   ami             = "ami-084568db4383264d4"
   instance_type   = "t2.micro"
   subnet_id       = aws_subnet.public_subnet.id
+  key_name        = "terraform"
   associate_public_ip_address = true
 
   tags = {
