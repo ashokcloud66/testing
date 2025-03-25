38fb8985fb26b15ce6a9ab8fb741bb06395d526b
ashokcloud66 <ashokcloud66@gmail.com>
2025-03-25 12:47:54 +0530

    3rd commit

--- subnet.tf
+++ subnet.tf
@@ -1,5 +1,5 @@
 resource "aws_subnet" "public_subnet" {
-  vpc_id            = aws_vpc.main.id
+  vpc_id            = aws_vpc.test_vpc.id
   cidr_block        = "10.0.0.0/24"
   map_public_ip_on_launch = true
 
